c565dd0d5f746dbcba24da08f1001e49b38ae14b
Bastien Dalla Piazza <bastien.dallapiazza@pix4d.com>
2019-05-03 13:08:52 +0200

    integrated gtest

--- src/mortonMatrix/tests/unit/tst_mortonMatrix.cpp
+++ src/mortonMatrix/tests/unit/tst_mortonMatrix.cpp
@@ -1,18 +1,15 @@
-#include <cstdlib>
-#include <cassert>
+#include <gtest/gtest.h>
+
 #include <mortonMatrix/mortonMatrix.hpp>
 
 using namespace mtmt;
 
-int main()
-{
-    std::vector<int> matrix = {0, 1, 2, 3};
-    reorder(matrix.data(), 2, 2);
+TEST(AMortonMatrix, indexingWorksOn2x2) {
+  std::vector<int> matrix = {0, 1, 2, 3};
+  reorder(matrix.data(), 2, 2);
 
-    assert(matrix[coord2Z(0, 0)] == 0);
-    assert(matrix[coord2Z(0, 1)] == 1);
-    assert(matrix[coord2Z(1, 0)] == 2);
-    assert(matrix[coord2Z(1, 1)] == 3);
-
-    return EXIT_SUCCESS;
+  ASSERT_EQ(matrix[coord2Z(0, 0)], 0);
+  ASSERT_EQ(matrix[coord2Z(0, 1)], 1);
+  ASSERT_EQ(matrix[coord2Z(1, 0)], 2);
+  ASSERT_EQ(matrix[coord2Z(1, 1)], 3);
 }
