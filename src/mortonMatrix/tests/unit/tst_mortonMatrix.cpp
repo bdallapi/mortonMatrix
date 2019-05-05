ab2663092f28e65bd8737bab1392f42965265ee0
Bastien Dalla Piazza <bastien.dallapiazza@pix4d.com>
2019-05-05 12:20:09 +0200

    more UTs

--- src/mortonMatrix/tests/unit/tst_mortonMatrix.cpp
+++ src/mortonMatrix/tests/unit/tst_mortonMatrix.cpp
@@ -4,6 +4,24 @@
 
 using namespace mtmt;
 
+struct AMortonCode : public testing::TestWithParam<std::pair<uint16_t, uint16_t>>
+{
+};
+
+TEST_P(AMortonCode, codeAndDecodeIsConsistent)
+{
+    ASSERT_EQ(mtmt::z2Coord(mtmt::coord2Z(GetParam().first, GetParam().second)), GetParam());
+}
+
+INSTANTIATE_TEST_SUITE_P(SomeCoordinates, AMortonCode,
+                         testing::Values(std::pair<uint16_t, uint16_t>(0, 0),
+                                         std::pair<uint16_t, uint16_t>(1, 0),
+                                         std::pair<uint16_t, uint16_t>(0, 1),
+                                         std::pair<uint16_t, uint16_t>(17, 42),
+                                         std::pair<uint16_t, uint16_t>(64, 64),
+                                         std::pair<uint16_t, uint16_t>(2042, 1017)
+                                         ));
+
 TEST(AMortonMatrix, indexingWorksOn2x2) {
   std::vector<int> matrix = {0, 1, 2, 3};
   reorder(matrix.data(), 2, 2);
